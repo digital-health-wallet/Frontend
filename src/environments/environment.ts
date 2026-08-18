ee153024376dfa1f25483a0e761eca1e23a0deed
Gabriela Viana Cunha <gabiviana.cunha@gmail.com>
2026-08-17 21:30:20 -0300

    fix(config): update backend api host and cache index.html on nginx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // IP da máquina rodando o backend na rede local atual: atualize ao trocar de rede.
 export const environment = {
   production: false,
-  apiHost: 'http://192.168.15.3:8080',
+  apiHost: 'http://172.100.127.70:8080',
 };
